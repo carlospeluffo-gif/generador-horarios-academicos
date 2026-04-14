c14b34cb0ac03b9ad0535a90e386735a2ba0f291
carlospeluffo-gif <carlos.peluffo@upr.edu>
2026-04-14 09:15:50 -0400

    app.py

--- app.py
+++ app.py
@@ -403,6 +403,10 @@ PATRONES = {
         {"name": "Ma-Ju", "days": {"Ma": 1.5, "Ju": 1.5}},
         {"name": "Ma (Intensivo)", "days": {"Ma": 3}},
         {"name": "Ju (Intensivo)", "days": {"Ju": 3}},
+        {"name": "Lu (Intensivo)", "days": {"Lu": 3}},
+        {"name": "vi (Intensivo)", "days": {"vi": 3}},
+        {"name": "Mi (Intensivo)", "days": {"Mi": 3}},
+        
     ],
     4: [
         {"name": "Lu-Ma-Mi-Ju", "days": {"Lu": 1, "Ma": 1, "Mi": 1, "Ju": 1}},
